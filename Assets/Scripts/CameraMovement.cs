a600ffb0f488d60833eddeacd9d9a70bb829ec69
traizooo <michal.owsiejew23@gmail.com>
2024-11-25 13:55:40 +0000

    Update damage amount in SampleScene and adjust camera smooth time for improved movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,7 +4,7 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
-    private float smoothTime = 0.25f;
+    private float smoothTime = 0.5f;
     private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
